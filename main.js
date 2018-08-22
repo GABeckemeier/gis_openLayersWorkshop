edb3f166b5921b6629c8ce74d381081cd8dbed45
GBeckemeier <gabbybeckemeier@gmail.com>
2018-08-22 13:03:47 -0500

    Vector Tiles section: Interact with VectorTile features code

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
 import 'ol/ol.css';
-import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import sync from 'ol-hashed';
-import DragAndDrop from 'ol/interaction/DragAndDrop';
-import Modify from 'ol/interaction/Modify';
-
-const source = new VectorSource();
+import MVT from 'ol/format/MVT';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+import Overlay from 'ol/Overlay';
+/*The below code adds a listener for clicks on the map,
+queries features at the clicked position and displays a popup with attributs of each feature*/
+const key ='lfsrAFTAV6x6mvNLFoAO';
 
 const map = new Map({
   target: 'map-container',
-  const layer = new VectorLayer({
-    source: source
-  });
-  map.addLayer(layer);
-  map.addInteraction(new DragAndDrop({
-    source: source,
-    formatConstructors: [GeoJSON]
-  }));
   view: new View({
     center: [0, 0],
     zoom: 2
   })
 });
-sync(map);
-map.addInteraction(new Modify({
-  source: source
-}));
+
+const layer = new VectorTileLayer({
+  source: new VectorTileSource({
+    attributions: [
+      '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>',
+      '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
+    ],
+    format: new MVT(),
+    url: `https://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}`,
+    maxZoom: 14
+  })
+});
+map.addLayer(layer);
+
+const overlay = new Overlay({
+  element: document.getElementById('popup-container'),
+  positioning: 'bottom-center',
+  offset: [0, -10],
+  autoPan: true
+});
+map.addOverlay(overlay);
+overlay.getElement().addEventListener('click', functoin() {
+  overlay.setPosition();
+});
+
+map.on('click', function(e) {
+  let markup = '';
+  map.forEachFeatureAtPixel(e.pixel, function(feature) {
+    markup += `${markup && '<hr>'}<table>`;
+    const properties = feature.getProperties();
+    for (const property in properties) {
+      markup += `<tr><th>${property}</th><td>${properties[property]}</td></tr>`;
+    }
+    markup += '</table';
+  }, {hitTolerance: 1});
+  if (markup) {
+    document.getElementById('popup-content').innerHTML = markup;
+    overlay.setPosition(e.coordinate);
+  } else {
+    overlay.setPosition();
+  }
+});
+//after setting this step up, styles can be created using the popup information
